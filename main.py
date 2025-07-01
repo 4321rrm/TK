f346f0f776c30726aeb7b33f6acf995f00f565c4
4321rrm <121351444+4321rrm@users.noreply.github.com>
2025-07-01 22:48:49 +0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,43 @@
+# from tkinter import ttk
+import tkinter as tk
+from tkinter import *
+from tkinter import ttk
+
+root=tk.Tk()
+root.title("Пример списков")
+root.geometry("500x500")
+
+def button_click1():
+    l1=Label(text="нажата кнопка1")
+    label.pack()
+
+def button_click2():
+    l2=Label(text="нажата кнопка2")
+    label.pack()
+
+button1 = tk.Button(
+root,
+text="Кнопка1",
+command=button_click1,
+bg="lightblue",  # Background color
+fg="darkblue",  # Foreground (text) color
+font=("Arial", 14, "bold"),  # Font style and size
+padx=20,  # Horizontal padding
+pady=10
+)
+button1.pack()
+
+button2 = tk.Button(
+root,
+text="кнопка2",
+command=button_click2,
+bg="lightblue",  # Background color
+fg="darkblue",  # Foreground (text) color
+font=("Arial", 14, "bold"),  # Font style and size
+padx=20,  # Horizontal padding
+pady=10
+)
+button2.pack()
+
+
+root.mainloop()
